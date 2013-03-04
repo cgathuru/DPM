2afb7579f4b5c11f184942cbb840f9b418ec0173
Charles Gathuru <charles.gathuru@mail.mcgill.ca>
2013-03-04 17:04:44 -0500

    bluetoth capability

---
+++ src/communication/bluetooth/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package allows for a remote device to communicate instructions to the NXT brick
+ * such as the corner it is in, the location of the ball dispenser and the goal, among
+ * other parameters required for the robot to function. 
+ */
+/**
+ * @author charles
+ *
+ */
+package communication.bluetooth;
