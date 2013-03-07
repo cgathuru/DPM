167839bcc2b5bca9e5a1854ca516b8b7e14a86de
Charles Gathuru <charles.gathuru@mail.mcgill.ca>
2013-03-07 18:16:22 -0500

    added zach's locolization test mod

--- tests/tests/TestLocalization.java
+++ tests/tests/TestLocalization.java
@@ -1,7 +1,11 @@
 package tests;
 
+import display.LCDInfo;
+import navigaion.Navigation;
+import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.SensorPort;
+import lejos.nxt.Sound;
 import lejos.nxt.UltrasonicSensor;
 import odometer.Odometer;
 import odometer.TwoWheeledRobot;
@@ -15,9 +19,14 @@ public class TestLocalization {
 	public static void main(String[] args) {
 		TwoWheeledRobot patBot = new TwoWheeledRobot(Motor.A, Motor.B);
 		Odometer odo = new Odometer(patBot, true);
+		Navigation navigator = odo.getNavigation();
 		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S3);
 		USLocalizer usl = new USLocalizer(odo, us);
+		Button.waitForAnyPress();
+		new LCDInfo(odo);
 		usl.doLocalization();
+		navigator.turnTo(90);
+		Sound.beepSequenceUp();
 
 	}
 
