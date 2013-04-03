800e6774dfc89ed1ec8c2237f6b79fa117f62e61
Charles Gathuru <charles.gathuru@mail.mcgill.ca>
2013-04-03 14:40:33 -0400

    added javadocs stuff

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -79,20 +79,27 @@ public final class Constants {
 	 * method will return false.
 	 */
 	public static final int CONSECUTIVE_DARK_LINES = 5; //number of consecutive dark lines allowed
+	/**
+	 * Distance used by the Ultrasonic sensor to determine if the wall is detected
+	 */
 	public static final int WALL_DIST = 60; //for ultrasonic sensor
 	public static final int US_ANGLE_OFFSET = -26;
+	/**
+	 * The amount of time passed before the {@code OdometryCorrection} is ran again
+	 */
 	public static final int ODOMETER_CORRECTION_TIMEOUT = 20; //number of miliseconds after which correction is called again
+	/**
+	 * The distance used by {@link OdometryCorrection} when determining which line the sensor has detected
+	 */
 	public static final double ODOMETRY_CORRECTION_MAX_ERROR_ALLOWANCE = WIDTH/2;
+	/**
+	 * Difference between the two 'same' values by each light sensor
+	 */
 	public static final double MAX_LINE_CORRECTION_BANDWIDTH = 1; //difference between the two 'same' values by each light sensor
+	/**
+	 * Amount of time before a detected line is removed from the stack in {@link OdometryCorrection}
+	 */
 	public static final long TIME_REFRESH_CONSTANT = 1500; //amount of time passed before line latched is removed from stack
-	public static final int WALL_FOLLWER_REFRESH = 50; //the amount of times passed before the us sensor is re-pinged
-	public static final int WALL_FOLLOWER_BANDWIDTH = 3;
-	public static final int WALL_FOLLOWER_BANDCENTER = 28; 
-	public static final int WALL_FOLLOWER_DELTA = 25; //incremental increase/decrease to wheels speed.
-	public static final int WALL_FOLLOWER_FILTER_OUT = 20; //number of readings before considered no wall present
-	public static final int WALL_FOLLOWER_MAX_SPEED = 200; //max speed to move wheel at
-	public static final int WALL_FOLLOWER_MIN_SPEED = 60;  //min speed to move wheel at
-	public static final String SLAVE_NAME = "Android16";
 	public static final int ObstacleDist = 25; 
 	public static final int motorLow = 170;
 	public static final int clearPathDist=80;
