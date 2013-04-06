bbd9f63444128c1b26ff289eecd17dc24a8132a2
Zachary Zoldan <zzoldan@gmail.com>
2013-04-06 18:58:25 -0400

    Madeleine's obstacle avoidance

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -101,7 +101,7 @@ public final class Constants {
 	 */
 	public static final long TIME_REFRESH_CONSTANT = 1500; //amount of time passed before line latched is removed from stack
 	
-	public static final int ObstacleDist = 25; 
+	public static final int ObstacleDist = 30; 
 	public static final int motorLow = 170;
 	public static final int clearPathDist=80;
 	 public static final int OBSTACLE_DIST = 30;
@@ -116,5 +116,31 @@ public final class Constants {
 	 public static final int NO_PATH_DIST = 45;
 	 public static final int MIN_TURN_TIME =1500;
 	 
+	 
+	 
+	  public static final int WALL_FOLLWER_REFRESH = 50; //the amount of times passed before the us sensor is re-pinged
+	  public static final int WALL_FOLLOWER_BANDWIDTH = 3;
+	  public static final int WALL_FOLLOWER_BANDCENTER = 28; 
+	  public static final int WALL_FOLLOWER_DELTA = 25; //incremental increase/decrease to wheels speed.
+	  public static final int WALL_FOLLOWER_FILTER_OUT = 20; //number of readings before considered no wall present
+	  public static final int WALL_FOLLOWER_MAX_SPEED = 200; //max speed to move wheel at
+	  public static final int WALL_FOLLOWER_MIN_SPEED = 60;  //min speed to move wheel at
+	  public static final String SLAVE_NAME = "Android16";
+	  public static final int SIDE_DISTANCE = 30;//distance to the right or left
+	  public static final int FORWARD_DISTANCE = 60; //forward distance
+	  public static final int OBSTACLE_DISTANCE_WHILE_AVOIDING = 20;
+	  public static final int OBSTACLE_NOT_THERE_DISTANCE = 90;
+	  public static final int BOTTOM_LEFT_X = -30;
+	  public static final int BOTTOM_LEFT_Y = -30;
+	  public static final int BOTTOM_RIGHT_X = 330;
+	  public static final int BOTTOM_RIGHT_Y = -30;
+	  public static final int TOP_LEFT_X = -30;
+	  public static final int TOP_LEFT_Y = 330;
+	  public static final int TOP_RIGHT_X = 330;
+	  public static final int TOP_RIGHT_Y = 330; 
 
-}
+	 }
+
+	 
+
+
--- src/navigation/Obstacle.java
+++ src/navigation/Obstacle.java
@@ -24,9 +24,8 @@ public class Obstacle {
  private Odometer odometer;   
  private TwoWheeledRobot robot;
  private boolean newObstacleDetected;//
- private int rightSensor,leftSensor;
- private int rightSideDist, leftSideDist;
- private boolean loop;
+ private int rightSensor,leftSensor; 
+ private boolean loop; 
  
  public Obstacle(UltrasonicSensor usRight, UltrasonicSensor usLeft, Odometer odometer, TwoWheeledRobot robot){  
   this.usLeft = usLeft;
@@ -37,76 +36,129 @@ public class Obstacle {
  
  public void avoid(int targetX, int targetY){ 
   newObstacleDetected = false;
-  loop = true;
+  loop = true;  
   
   while(leftUsDist()<Constants.OBSTACLE_DIST2||rightUsDist()<Constants.OBSTACLE_DIST2){ //back up when the robot is too close to an obstacle
    Motor.A.setSpeed(Constants.FORWARD_SPEED); //
    Motor.B.setSpeed(Constants.FORWARD_SPEED); // 
    Motor.A.backward();
    Motor.B.backward();
-  }  
+  } 
   
-//  robot.turnToImmediate(65);//look for free path
-//  rightSideDist = leftUsDist();
-//  robot.turnToImmediate(-135);
-//  leftSideDist = rightUsDist();
-//  robot.turnToImmediate(65);
+
   
-//  if(rightSideDist>leftSideDist){ 
+//  if(avoidanceRoutine() == 1){
+//   moveToTheRight(); //avoid by moving to the right
+//   while(loop){
+//    loop = false;
+//    while(newObstacleDetected/*||isObstacleStillThereR()*/){
+//     moveToTheRight();
+//     if (isHeadedToWall()){//if robot is close to wall stop avoiding 
+//      moveParallelToWall(targetX,targetY);
+//      return;
+//     }
+//    }
+//    moveForwardBy(Constants.FORWARD_DISTANCE);
+//    if (isHeadedToWall()){//if robot is close to wall stop avoiding 
+//     moveParallelToWall(targetX,targetY);
+//     return;
+//    }
+//    
+//    if(newObstacleDetected) loop = true;
+//    newObstacleDetected = false;
+//   }
+//  }else if(avoidanceRoutine() == 2){
+//   moveToTheLeft(); //avoid by moving to the left
+//   while(loop){
+//    loop = false;
+//    while(newObstacleDetected/*||isObstacleStillThereL()*/){
+//     moveToTheLeft();
+//     if (isHeadedToWall()){//if robot is close to wall stop avoiding 
+//      moveParallelToWall(targetX,targetY);
+//      return;
+//     }
+//    }
+//    moveForwardBy(Constants.FORWARD_DISTANCE);
+//    if (isHeadedToWall()){//if robot is close to wall stop avoiding 
+//     moveParallelToWall(targetX,targetY);
+//     return;
+//    }
+//    if(newObstacleDetected) loop = true;
+//    newObstacleDetected = false;
+//   }
+//  }    
+// }
   
   if(avoidanceRoutine() == 1){
-   moveToTheRight(); //avoid by moving to the right
-   while(loop){
-    loop = false;
-    while(newObstacleDetected||isObstacleStillThereR()) moveToTheRight();
-    moveForwardBy(60);
-    if(newObstacleDetected) loop = true;
-    newObstacleDetected = false;
+   newObstacleDetected = true;
+   
+   while(newObstacleDetected) {
+    moveToTheRight(Constants.SIDE_DISTANCE);
+    
+    while(!newObstacleDetected && isObstacleStillThereR()){
+     moveToTheRight(15);     
+    }
+    
+    moveForwardBy(Constants.FORWARD_DISTANCE);
+    if (isHeadedToWall()){//if robot is close to wall stop avoiding 
+     moveParallelToWall(targetX,targetY);
+     return;
+    }
    }
+   
+   
   }else if(avoidanceRoutine() == 2){
-   moveToTheLeft(); //avoid by moving to the left
-   while(loop){
-    loop = false;
-    while(newObstacleDetected||isObstacleStillThereL()) moveToTheLeft();
-    moveForwardBy(60);
-    if(newObstacleDetected) loop = true;
-    newObstacleDetected = false;
+   newObstacleDetected = true;
+   
+   while(newObstacleDetected) {
+    moveToTheLeft(Constants.SIDE_DISTANCE); 
+    
+    while(!newObstacleDetected && isObstacleStillThereL()){
+     moveToTheLeft(15);     
+    }
+    
+    moveForwardBy(Constants.FORWARD_DISTANCE);
+    if (isHeadedToWall()){//if robot is close to wall stop avoiding 
+     moveParallelToWall(targetX,targetY);
+     return;
+    }
    }
-  }    
+  } 
+      
  }
  
- private boolean isObstacleStillThereR(){//robot is to the right of the obstacle  
-  robot.turnToImmediate(-25);
+ private boolean isObstacleStillThereR(){ //robot is to the right of the obstacle  
+  robot.turnToImmediate(-Constants.US_ANGLE);
   leftSensor = leftUsDist();
-  robot.turnToImmediate(25);
+  robot.turnToImmediate(Constants.US_ANGLE);
   
-  if(leftSensor<90){
+  if(leftSensor<Constants.OBSTACLE_NOT_THERE_DISTANCE){
    return true;
   }
   return false;
  }
  
- private boolean isObstacleStillThereL(){// robot is to the left of the obstacle 
-  robot.turnToImmediate(25);
+ private boolean isObstacleStillThereL(){ // robot is to the left of the obstacle 
+  robot.turnToImmediate(Constants.US_ANGLE);
   rightSensor = rightUsDist();
-  robot.turnToImmediate(-25);  
+  robot.turnToImmediate(-Constants.US_ANGLE);  
   
-  if(rightSensor<90){
+  if(rightSensor<Constants.OBSTACLE_NOT_THERE_DISTANCE){
    return true;
   }
   return false;
  }
  
- private void moveToTheRight(){//avoid by moving to the right
+ private void moveToTheRight(int distance ){//avoid by moving to the right
   robot.turnToImmediate(90);
-  moveForwardBy(30);
+  moveForwardBy(distance);
   if(newObstacleDetected) return;
   robot.turnToImmediate(-90); 
- }
+ } 
  
- private void moveToTheLeft(){//avoid by moving to the left
+ private void moveToTheLeft(int distance){//avoid by moving to the left
   robot.turnToImmediate(-90);
-  moveForwardBy(30);
+  moveForwardBy(distance);
   if(newObstacleDetected) return;
   robot.turnToImmediate(90);  
  }
@@ -118,14 +170,14 @@ public class Obstacle {
   
   while(rightMotor.isMoving()){ //code to stop the motors when an obstacle is detected
    
-    if(leftUsDist()<20||rightUsDist()<20){
+    if(leftUsDist()<Constants.OBSTACLE_DISTANCE_WHILE_AVOIDING||rightUsDist()<Constants.OBSTACLE_DISTANCE_WHILE_AVOIDING){
      newObstacleDetected  = true;
       return;
     }
   }  
  } 
  
- private double headingTo(int x, int y){//used to calculate theta to corner
+ private double headingTo(int x, int y){//used to calculate heading to corner
   double dx = x-odometer.getX();
   double dy = y-odometer.getY();
   double headingTo = 90- Math.toDegrees(Math.atan2(dy, dx));//
@@ -135,21 +187,21 @@ public class Obstacle {
  //1 for avoiding by turning to the right and 2 for the left
  private int avoidanceRoutine(){
   
-  //turning to the right
+  //turning right
   double headingToTheRight = odometer.getTheta()+90;
   if(headingToTheRight>360) headingToTheRight-=360;
   
-  //turning to the left
+  //turning left
   double headingToTheLeft = odometer.getTheta()-90;
   if(headingToTheLeft<0) headingToTheLeft+=360;
   
-  //wall the robot is headed to by turning to the right
+  //wall the robot is headed to by turning right
   int wallR = wallpointedAt(headingToTheRight);
   
-  //wall the robot is headed to by turning to the left
+  //wall the robot is headed to by turning left
   int wallL = wallpointedAt(headingToTheLeft);
   
-  //
+  //return the routine that moves the robot away from the closest wall
   if(distanceToWall(wallR)>distanceToWall(wallL))
    return 1;
   else 
@@ -157,14 +209,14 @@ public class Obstacle {
     
  }
  
- //wall the robot is moving towards by turning to the left or right
+ //wall the robot is moving towards by turning left or right
  //bottom wall is 1, left wall is 2, top wall is 3, right wall is 4
  private int wallpointedAt(double heading){
   //headings to corners
-  double bottomLeftAng = headingTo(-30,-30);
-  double bottomRightAng = headingTo(330,-30);
-  double topLeftAng = headingTo(-30,330);
-  double topRightAng = headingTo(330,330);
+  double bottomLeftAng = headingTo(Constants.BOTTOM_LEFT_X,Constants.BOTTOM_LEFT_Y);
+  double bottomRightAng = headingTo(Constants.BOTTOM_RIGHT_X,Constants.BOTTOM_RIGHT_Y);
+  double topLeftAng = headingTo(Constants.TOP_LEFT_X,Constants.TOP_LEFT_Y);
+  double topRightAng = headingTo(Constants.TOP_RIGHT_X,Constants.TOP_RIGHT_Y);
   
   //if the heading is between the two bottom corners, return wall 1 i.e. bottom wall
   if((greaterThan(heading,bottomRightAng)||equalTo(heading,bottomRightAng)) && (lessThan(heading,bottomLeftAng)||equalTo(heading,bottomLeftAng))) 
@@ -189,7 +241,7 @@ public class Obstacle {
   else if(wallNumber == 4)
    return 330 - odometer.getX();
   
-  return 0;//not used for anything  
+  return 0;//if wall number is not valid  
  }
  
  //bottom wall is 1, left wall is 2, top wall is 3, right wall is 4
@@ -206,10 +258,84 @@ public class Obstacle {
   return 0; //robot not close to any wall  
  }
  
+ //returns true if the robot is close to a wall
+ private boolean isCloseToWall(){
+  if((odometer.getY()<0)||(odometer.getX()<0)||(odometer.getY()>300)||(odometer.getX()>300))
+   return true;
+  return false;   
+ }
+ 
+ //returns true if the robot is headed to a wall
+ private boolean isHeadedToWall(){
+  if (closeToWhichWall()==1 && (odometer.getTheta()>90 && odometer.getTheta()<270)) //headed to bottom wall
+   return true;
+  else if(closeToWhichWall()==2 && (odometer.getTheta()>180 && odometer.getTheta()<360)) //headed to left wall
+   return true;
+  else if(closeToWhichWall()==3 && ( (odometer.getTheta()>270 && odometer.getTheta()<=360) || (odometer.getTheta()>=0 && odometer.getTheta()<90))) //headed to top wall
+    return true;
+  else if(closeToWhichWall()==4 && (odometer.getTheta()>0 && odometer.getTheta()<180))
+   return true;
+  
+  return false;  
+ } 
+ 
+ private int isHeadedToWhichWall(){
+  if (closeToWhichWall()==1 && (odometer.getTheta()>90 && odometer.getTheta()<270)) //headed to bottom wall
+   return 1;
+  else if(closeToWhichWall()==2 && (odometer.getTheta()>180 && odometer.getTheta()<360)) //headed to left wall
+   return 2;
+  else if(closeToWhichWall()==3 && ( (odometer.getTheta()>270 && odometer.getTheta()<=360) || (odometer.getTheta()>=0 && odometer.getTheta()<90))) //headed to top wall
+    return 3;
+  else if(closeToWhichWall()==4 && (odometer.getTheta()>0 && odometer.getTheta()<180))
+   return 4;
+  
+  return 0; //not headed to wall  
+ }
+ 
+ //moves the robot parallel to the wall
+ private void moveParallelToWall(int xTarget,int yTarget){
+  
+  if(isHeadedToWhichWall() == 1){
+   if(odometer.getX()>xTarget){ //move to the left
+   robot.turnToFace(0,0);
+  
+   }else{
+    robot.turnToFace(300, 0);  
+   }
+  }else if(isHeadedToWhichWall() == 2){
+   if(odometer.getY()>yTarget){ //
+   robot.turnToFace(0,0);
+   
+   }else{
+    robot.turnToFace(0, 300);  
+   }
+  }
+  else if(isHeadedToWhichWall() == 3){
+   if(odometer.getX()>xTarget){
+    robot.turnToFace(0, 300);
+ 
+   }else{
+    robot.turnToFace(300, 300); 
+   }
+   
+  }else if(isHeadedToWhichWall() == 4){
+   if(odometer.getY()>yTarget){
+    robot.turnToFace(300, 0);
+ 
+   }else{
+    robot.turnToFace(300, 300);  
+   }
+   
+  }
+  moveForwardBy(30);
+  
+ }
+ 
+ 
  private void moveAwayFromWall(int x, int y){//takes in right or left corner coordinates
   robot.turnToFace(x, y);
   robot.moveForwardBy(30);  
- }
+ } 
    
  //returns true if angle1 is to the right of angle2 in a half circle
  private boolean greaterThan(double angle1, double angle2){
@@ -255,23 +381,17 @@ public class Obstacle {
  public static int leftUsDist(){ //filter code left out because it slows down detection
   
   // do a ping
- //usLeft.ping();   
- return usLeft.getDistance();  
+ usLeft.ping();   
+ leftUsDist = usLeft.getDistance();  
+ return leftUsDist;
  }
 
  public static int rightUsDist(){ 
  
   // do a ping
- //usRight.ping();   
- return usRight.getDistance();  
+ usRight.ping();   
+ rightUsDist = usRight.getDistance();  
+ return rightUsDist;
  }
- 
-// public static UltrasonicSensor getLeftUs(){
-//	 return usLeft;
-// }
-// 
-// public static UltrasonicSensor getRightUs(){
-//	 return usRight;
-// }
  
 }
